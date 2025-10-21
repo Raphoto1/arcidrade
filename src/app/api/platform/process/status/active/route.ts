05b6454aee977e04d21eae9483bb35e44cb9df1e
¨Rafael <rrhhmmtt@gmail.com>
2025-10-21 14:23:39 -0500

    error en status active corregido

--- src/app/api/platform/process/status/active/route.ts
+++ src/app/api/platform/process/status/active/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getAllActiveProcesses } from "@/controller/process.controller";
 
 export async function GET(request: NextRequest) {
-    const processList = await getAllActiveProcesses('active');
+    const processList = await getAllActiveProcesses();
     // console.log(processList);
     return NextResponse.json({ message: "Process data success" , payload:processList});
 }
